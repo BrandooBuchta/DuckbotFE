c16161751d91d8937228b5f5c5791822a86daf64
BrandooBuchta <buchta@brandoo.cz>
2025-05-27 20:23:29 +0200

    rebuild

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,5 @@
 import { FC, JSX, useEffect } from "react";
 import {
-  IconCheck,
-  IconCopy,
   IconLink,
   IconLogout,
   IconMessage,
@@ -11,18 +9,8 @@ import {
   IconVariable,
 } from "@tabler/icons-react";
 import Link from "next/link";
-import {
-  ActionIcon,
-  Box,
-  Button,
-  CopyButton,
-  Divider,
-  Text,
-  Tooltip,
-} from "@mantine/core";
+import { Box, Text } from "@mantine/core";
 import { useRouter } from "next/router";
-
-import Section from "./Section";
 
 import useBotStore from "@/stores/bot";
 
@@ -71,21 +59,6 @@ const Sidebar: FC<{ children: JSX.Element }> = ({ children }) => {
     </Link>
   ));
 
-  const links = data.map((item) => (
-    <a
-      key={item.label}
-      className={`flex items-center text-sm font-medium text-gray-700 dark:text-gray-200 px-3 py-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-black dark:hover:text-white transition-colors ${
-        query.section === item.link && pathname.includes("docs")
-          ? "text-pink-500"
-          : ""
-      }`}
-      href={`/docs/${item.link}`}
-    >
-      <item.icon className="mr-2 w-5 h-5 text-pink-500" stroke={1.5} />
-      <span>{item.label}</span>
-    </a>
-  ));
-
   return (
     <div className="relative">
       <header className="border-b h-20 border-gray-300 flex justify-between p-5 items-center fixed z-20 w-full bg-white">
@@ -108,65 +81,9 @@ const Sidebar: FC<{ children: JSX.Element }> = ({ children }) => {
           <div className="flex-1">
             <Text fw="bold">Settings</Text>
             {settings}
-            <Divider className="my-2" />
-            <Text fw="bold" pt="10">
-              Docs
-            </Text>
-            {links}
           </div>
         </nav>
-        <div className="ml-72 w-full mt-20">
-          {!pathname.includes("docs") && bot && (
-            <div className="gap-3 flex flex-col relative p-5 w-full">
-              <Section title="Info">
-                <div className="flex w-full justify-between items-center">
-                  <div>
-                    <p className="font-bold">URL</p>
-                    <div className="flex">
-                      <p className="text-gray-500 text">{`https://${bot.domain || "ducknation.vercel.app"}/${bot.isEvent ? "event" : "b"}/${encodeURIComponent(bot[bot.isEvent ? "eventName" : "name"])}`}</p>
-                      <CopyButton
-                        timeout={2000}
-                        value={`https://${bot.domain || "ducknation.vercel.app"}/${bot.isEvent ? "event" : "b"}/${encodeURIComponent(bot[bot.isEvent ? "eventName" : "name"])}`}
-                      >
-                        {({ copied, copy }) => (
-                          <Tooltip
-                            withArrow
-                            label={copied ? "Copied" : "Copy"}
-                            position="right"
-                          >
-                            <ActionIcon
-                              color={copied ? "teal" : "gray"}
-                              variant="subtle"
-                              onClick={copy}
-                            >
-                              {copied ? (
-                                <IconCheck size={16} />
-                              ) : (
-                                <IconCopy size={16} />
-                              )}
-                            </ActionIcon>
-                          </Tooltip>
-                        )}
-                      </CopyButton>
-                    </div>
-                  </div>
-                  <div className="flex gap-1">
-                    <Button
-                      variant="light"
-                      onClick={() =>
-                        webhookInfo ? deleteWebhook() : setWebhook()
-                      }
-                    >
-                      <p className="text-black mr-2">WEBHOOK: </p>
-                      <b>{webhookInfo ? "ON" : "OFF"}</b>
-                    </Button>
-                  </div>
-                </div>
-              </Section>
-            </div>
-          )}
-          {children}
-        </div>
+        <div className="ml-72 w-full mt-20">{children}</div>
       </div>
     </div>
   );
